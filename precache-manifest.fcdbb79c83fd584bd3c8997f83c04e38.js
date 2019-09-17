8ff89d17e47b64aa73c2d491c80b66fd23869b4f
Sara McMullin <mcsara1221@gmail.com>
2019-09-17 18:28:49 -0500

    Updates

---
+++ precache-manifest.fcdbb79c83fd584bd3c8997f83c04e38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739e11190941d466011597d89b2fa4ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5407ff99d6b33ab28a",
+    "url": "/static/css/main.103db8b6.chunk.css"
+  },
+  {
+    "revision": "1387db8877eb9bdf1fc6",
+    "url": "/static/js/2.0230eccd.chunk.js"
+  },
+  {
+    "revision": "ed5407ff99d6b33ab28a",
+    "url": "/static/js/main.410f9e42.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
