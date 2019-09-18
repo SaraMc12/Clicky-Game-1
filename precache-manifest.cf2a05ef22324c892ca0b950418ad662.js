68e00dcc02aa62f491ce1c2e4772f5392130696c
Sara McMullin <mcsara1221@gmail.com>
2019-09-17 19:33:01 -0500

    Updates

---
+++ precache-manifest.cf2a05ef22324c892ca0b950418ad662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596f57ac75493f41942c4665bc0fb9a8",
+    "url": "/Clicky-Game-1/index.html"
+  },
+  {
+    "revision": "412af31d0bdec0523bec",
+    "url": "/Clicky-Game-1/static/css/main.103db8b6.chunk.css"
+  },
+  {
+    "revision": "1387db8877eb9bdf1fc6",
+    "url": "/Clicky-Game-1/static/js/2.0230eccd.chunk.js"
+  },
+  {
+    "revision": "412af31d0bdec0523bec",
+    "url": "/Clicky-Game-1/static/js/main.42ee075f.chunk.js"
+  },
+  {
+    "revision": "cbc9901fb473fcbb3ee3",
+    "url": "/Clicky-Game-1/static/js/runtime~main.c11b04a0.js"
+  }
+]);
--- src/components/Title/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Title(props) {
-  return <h1 className="title">{props.topic} - Score: {props.score} | topScore: {props.total}</h1>;
-}
-
-export default Title;
